e3d256d0ac06653ec4b83e2abb70a3bb4221a42c
Omar Shekfeh <o.shekfeh@gmail.com>
2019-09-18 01:14:16 +0300

    Develop models for NewsCollector and NewsSource

--- newsaggregator/models.py
+++ newsaggregator/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+SOURCES_NAMES = (
+    ('reddit', 'Reddit'),
+    ('newsapi', 'NewsAPI')
+)
+
+#News sources class, subclasses and managers
+class RedditManager(models.Manager):
+    def get_queryset(self):
+        return super(RedditManager, self).get_queryset().filter(
+            source_name='reddit')
+
+class NewsAPIManager(models.Manager):
+    def get_queryset(self):
+        return super(NewsAPIManager, self).get_queryset().filter(
+            source_name='newsapi')
+
+class NewsSource(models.Model):
+    source_name = models.CharField(max_length=200, choices=SOURCES_NAMES, unique=True)
+
+
+class Reddit(NewsSource):
+    objects = RedditManager()
+
+    def fetch_news(self):
+        print('Fetch reddit news')
+
+    class Meta:
+        proxy = True
+
+class NewsAPI(NewsSource):
+    objects = NewsAPIManager()
+
+    def fetch_news(self):
+        print('Fetch NewsAPI news')
+
+    class Meta:
+        proxy = True
---
+++ newsaggregator/utilites.py
@@ -0,0 +1,21 @@
+from newsaggregator.models import NewsSource, Reddit, NewsAPI
+
+class NewsCollector():
+    def __init__(self):
+        print("Collector initiated")
+
+    def fetch_news(self):
+        SOURCE_TYPE_TO_CLASS_MAP = {
+            'reddit':'Reddit',
+            'newsapi':'NewsAPI'
+        }
+        sources = NewsSource.objects.all()
+        news = []
+        for source in sources:
+            class_name = SOURCE_TYPE_TO_CLASS_MAP[source.source_name]
+            source_news = globals()[class_name](source).fetch_news(query)
+            news.extend(source_news)
+
+        #sort news by date from old to new
+        from operator import itemgetter
+        return sorted(news, key=itemgetter('date'), reverse=True)
