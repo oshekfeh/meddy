c41f610ac4116e4b30a30ce2941f609515e21436
Omar Shekfeh <o.shekfeh@gmail.com>
2019-09-18 01:31:51 +0300

    Add simple unit test method. Aggregate needed python library in requirements.txt

--- newsaggregator/tests.py
+++ newsaggregator/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from . import views
 
 # Create your tests here.
+class APITests(APITestCase):
+  def test_success_working_api(self):
+    url = reverse('newsaggregator:list')
+    response = self.client.get(url)
+    assert response.status_code == 200
